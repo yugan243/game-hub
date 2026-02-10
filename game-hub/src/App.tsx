e1fb3b12363b70b4861c8650ed02e0194b250c2c
yugan243 <yugannimsara@gmail.com>
2026-02-10 20:39:11 +0530

    Filter games by platform

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -5,9 +5,11 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import type { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import type { Platform } from "./hooks/useGames";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
   return (
     <Grid
       templateAreas={{
@@ -26,8 +28,8 @@ function App() {
         <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
       </GridItem>
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre}></GameGrid>
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform)=> setSelectedPlatform(platform)} />
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}></GameGrid>
       </GridItem>
     </Grid>
   );
--- game-hub/src/components/PlatformSelector.tsx
+++ game-hub/src/components/PlatformSelector.tsx
@@ -1,7 +1,12 @@
+import type { Platform } from "@/hooks/useGames";
 import usePlatforms from "@/hooks/usePlatforms";
 import { Menu, Button, Portal } from "@chakra-ui/react";
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatform: Platform | null;
+}
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
     const { data, error} = usePlatforms();
 
     if (error) return null;
@@ -9,15 +14,15 @@ const PlatformSelector = () => {
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="outline" size="sm">
-          Platforms
+          {selectedPlatform?.name || 'Platforms'}
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            {data.map(platform => <Menu.Item value={platform.name}>{platform.name}</Menu.Item>)}
+            {data.map(platform => <Menu.Item onClick={() => onSelectPlatform(platform)} value={platform.name} key={platform.id}>{platform.name}</Menu.Item>)}
           </Menu.Content>
-        </Menu.Positioner>
+        </Menu.Positioner> 
       </Portal>
     </Menu.Root>
   );
